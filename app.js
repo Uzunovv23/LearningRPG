ecfc6216282bf9146d0ff1c121378ec282f1f4df
Nikolay Uzunov <nikiuzunovv1122@gmail.com>
2025-12-08 19:23:25 +0200

    setup db

--- app.js
+++ app.js
@@ -10,6 +10,16 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var db = require('./models');
+
+db.sequelize.sync()
+  .then(() => {
+    console.log('Connection to MySQL has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  });
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
