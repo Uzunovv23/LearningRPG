4c7be9bb7fbd57bf37b55726e727f507b40f9828
Nikolay Uzunov <nikiuzunovv1122@gmail.com>
2026-01-22 09:12:50 +0200

    refactor of user profile + add display of purchased grades

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,15 @@
 var express = require("express");
 var router = express.Router();
+
 var userController = require("../controllers/userController");
 
-const isAuthenticated = (req, res, next) => {
-  if (req.isAuthenticated()) {
-    return next();
-  }
-  res.redirect("/login");
-};
+var isLogged = require("../middleware/isLogged");
 
 /* GET users listing. */
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
 });
 
-router.get("/my-hero", isAuthenticated, userController.getMyHero);
+router.get("/my-hero", isLogged, userController.show);
 
 module.exports = router;
